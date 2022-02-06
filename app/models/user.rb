d357cb685b645f91c65e4ad7c051c1f5fb25ab2a
wataken1356 <watanabekenya2020@gmail.com>
2022-02-06 16:37:07 +0900

    new board作成ページとboard indexページを途中まで作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :boards, dependent: :destroy
+
+  def has_written?(board)
+    boards.exists?(id: board.id)
+  end
+
 end
